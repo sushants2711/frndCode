d1a03020ba3f9f492da57f06f0c888471a9ac698
Sushant Singh <sushantsingh@Sushants-MacBook-Air.local>
2026-01-13 15:16:08 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: "https://photo-share-frontend-chi.vercel.app",
+    origin: "https://medilink-backend-mu.vercel.app",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"]
